4581d56c216ecc2284d9e40d62fd3c4725b43b56
aviralpanwar-130 <aviralpanwar263@gmail.com>
2023-04-30 19:27:06 +0530

    response

--- WelcomeNode.js
+++ WelcomeNode.js
@@ -1,6 +1,38 @@
 let http = require('http');
 let server = http.createServer(function(req,res){
-    console.log('Aviral Panwar');
+    let url = req.url;
+    
+    if(url==='/home'){
+        res.write('<html>');
+        res.write('<head><title>Home Page</title></head>');
+        res.write('<body><p>Welcome to home page</p></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    else if(url==='/about'){
+        res.write('<html>');
+        res.write('<head><title>About Page</title></head>');
+        res.write('<body><p>Welcome to about us page</p></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    else if(url==='/node'){
+        res.write('<html>');
+        res.write('<head><title>Node Page</title></head>');
+        res.write('<body><p>Welcome to node page</p></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    else{
+        res.write('<html>');
+        res.write('<head><title>Welcome Page</title></head>');
+        res.write('<body><p>Welcome to welcome page</p></body>');
+        res.write('</html>');
+        return res.end();
+    }
 })
 
 server.listen(4000);

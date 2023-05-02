cff3c12b09d038577db4dbe9c96fc3417389912f
aviralpanwar-130 <aviralpanwar263@gmail.com>
2023-05-02 21:29:55 +0530

    Express

--- WelcomeNode.js
+++ WelcomeNode.js
@@ -1,10 +1,24 @@
 const http = require('http');
 
-const routes = require('./routes.js');
+const express = require('express');
+
+//const routes = require('./routes.js');
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('Hello Avii');
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log('Hello Anjali');
+    res.send(`<h1>Hii express</h1>`);
+});
 
 
-const server = http.createServer(routes);
-server.listen(4000);
+
+app.listen(4000);
 
 
 

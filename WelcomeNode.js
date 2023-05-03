702c809850b76e8233633632d3e8657795ad4ca9
aviralpanwar-130 <aviralpanwar263@gmail.com>
2023-05-03 10:38:34 +0530

    Routes

--- WelcomeNode.js
+++ WelcomeNode.js
@@ -1,18 +1,29 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //const routes = require('./routes.js');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('Hello Avii');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req,res,next)=>{
+    console.log(`Hii I am mortal`);
     next();
 });
 
-app.use((req,res,next)=>{
-    console.log('Hello Anjali');
+app.use('/add-product',(req,res,next)=>{
+    res.send(`<form action="/product" method="POST"><input type="text" name="title"><button>Submit</button></form>`);
+});
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
     res.send(`<h1>Hii express</h1>`);
 });
 
